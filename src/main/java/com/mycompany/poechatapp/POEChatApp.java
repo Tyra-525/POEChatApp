99eafacf6fdcd0230345769fd22adbc15ae67a8f
tyra <Tyrazama073@gmail.com>
2026-09-10 00:54:13 +0200

    Menu Option

--- src/main/java/com/mycompany/poechatapp/POEChatApp.java
+++ src/main/java/com/mycompany/poechatapp/POEChatApp.java
@@ -3,14 +3,38 @@
  */
 
 package com.mycompany.poechatapp;
-
+import java.util.Scanner;
 /**
  *
  * @author tyraz
  */
 public class POEChatApp {
-
+static AccountValidation registeredAccount;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+         Scanner input = new Scanner(System.in);
+boolean running = true;
+System.out.println("******************************");
+System.out.println("Welcome to ChatApp");
+System.out.println("******************************");
+System.out.println("Please select an option below.");
+System.out.println("1.Register\n"
+                  + "2.Login\n"
+                  + "3.Exit");
+
+int option = input.nextInt();
+            input.nextLine(); // consume leftover newline
+
+            if (option == 1) {
+                register(input);
+            } else if (option == 2) {
+                login(input);
+            } else if (option == 3) {
+                System.out.println("\nGoodbye!");
+                running = false;
+            } else {
+                System.out.println("\nInvalid option. Please try again.\n");
+            }
     }
 }
+    
+
