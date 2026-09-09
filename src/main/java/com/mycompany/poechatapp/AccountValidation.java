f027d7f32d6a981fabc1e08622918367fc8e5ee0
tyra <Tyrazama073@gmail.com>
2026-09-10 00:59:31 +0200

    account validation method

--- src/main/java/com/mycompany/poechatapp/AccountValidation.java
+++ src/main/java/com/mycompany/poechatapp/AccountValidation.java
@@ -9,5 +9,28 @@ package com.mycompany.poechatapp;
  * @author tyraz
  */
 public class AccountValidation {
-    
+    public class AccountValidation {
+   public String name;
+   public String username; 
+   public String password;
+   public String cellPhoneNumber;
+//Getters
+public AccountValidation(){}
+public String getName(){return name;}
+public String getUsername(){return username;}
+public String getPassword(){return password;}
+public String getCellPhoneNumber(){return cellPhoneNumber;}
+
+ 
+
+//method for name
+public boolean name(String name){ //if theres a problem its because you exlcded the name cannot be empty 
+if (!name.trim().matches("[ a-zA-Z]+")){
+System.out.println("First name must contain letters or numbers");
+return false;
+}
+this.name = name.trim();
+return true;
+}
+
 }
